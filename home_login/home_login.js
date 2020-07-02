9c897faccc22c48c416758944d875da5dbcfcdf7
Hao Xin Chao <jason_c0123@hotmail.com>
2020-07-01 22:47:15 -0400

    Created visitor versions of leaderboard and patchnote page, hardcoded login credential checks

--- home_login/home_login.js
+++ home_login/home_login.js
@@ -1,7 +1,26 @@
+document.getElementById("signin-form").addEventListener("submit", checkCredentials);
+
+
+// A function to check the credential of the user
+// Should've sent credentials to user then obtain what to do from server
+function checkCredentials(e) {
+    e.preventDefault();
+    const username = document.getElementById("username-1").value;
+    const password = document.getElementById("password-1").value;
+    if (username == "user" && password == "user") {
+        open("../gameplay.html");
+    } else if (username == "admin" && password == "admin") {
+        // open admin panel
+    } else {
+        alert("Incorrect username or password!")
+    }
+}
+
+// toggles between signin and login form
 $(function() {
     $(".form-toggle").on("click", function(e) {
         e.stopImmediatePropagation();
-        
+
         $(this).closest("#form-module-container").toggleClass("sign-in");
     });
 });
