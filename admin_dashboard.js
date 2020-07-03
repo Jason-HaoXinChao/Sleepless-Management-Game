8d176f43baf697d6e959e2b35206582553ec990b
Kevin Mo <kevin.hy.mo@gmail.com>
2020-07-02 21:08:16 -0400

    Added popup module to confirm admin changes to a user's info

--- admin_dashboard.js
+++ admin_dashboard.js
@@ -1,11 +1,69 @@
+'use strict';
+
+$("#remove-user-icon").on("click", function() {
+    $(this).prev("#user-icon").children("img").attr("src", "home_login/visitor_icon.png")
+});
+
+$("#edit-username").on("click", function() {
+    $("#username").attr("contenteditable", $(this).prev("span").attr("contenteditable") == 'false');
+});
+
 $(".change-stat").on("click", function() {
     $(this).prevAll(".stat:first").text(Math.max(0, Math.min(100, parseInt($(this).prevAll(".stat:first").text()) + ($(this).hasClass("decrease-stat") ? -1 : 1))));
 });
 
-$(".edit-user-info").on("click", function() {
-    $(this).prev("span").attr("contenteditable", $(this).prev("span").attr("contenteditable") == 'false');
+$("#save-changes").on("click", function() {
+    if ($("input#ban-user-checkbox").is(":checked")) {
+        $("li#ban-user-confirm").addClass("active");
+    }
+
+    if ($("#user-icon > img").attr("src") != $("#user-icon > img").attr("data-original")) {
+        $("li#remove-user-icon-confirm").addClass("active");
+    }
+
+    if ($("#username").text() != $("#username").attr("data-original")) {
+        $("li#change-username-confirm").addClass("active");
+    }
+
+    $(".stat").each(function() {
+        if ($(this).text() != $(this).attr("data-original")) {
+            $("li#change-stats-confirm").addClass("active");
+        }
+    });
+
+    if ($("li.active").length) {
+        $("#popup-module-blackout").addClass("active");
+    } else {
+        $("#no-changes").addClass("active");
+
+        setTimeout(function() {
+            $("#no-changes").removeClass("active");
+        }, 2000);
+    }
 });
 
-$("#remove-user-icon").on("click", function() {
-    $(this).prev("#user-icon").children("img").attr("src", "home_login/visitor_icon.png")
+$("#confirm-changes").on("click", function() {
+    $("*[data-original]").each(function() {
+        if ($(this).is("img")) {
+            $(this).attr("data-original", $(this).attr("src"));
+        } else {
+            $(this).attr("data-original", $(this).text());
+        }
+    });
+
+    $(this).closest("#popup-module-blackout").removeClass("active");
+
+    $("li").each(function() {
+        $(this).removeClass("active");
+    });
+});
+
+$("#revert-changes").on("click", function() {
+    $("*[data-original]").each(function() {
+        if ($(this).is("img")) {
+            $(this).attr("src", $(this).data("original"));
+        } else {
+            $(this).text($(this).data("original"));
+        }
+    });
 });
