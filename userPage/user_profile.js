3aca24449d86442f72d008911cad3f0c9d9f112c
ParekhDevanshi <43529601+ParekhDevanshi@users.noreply.github.com>
2020-07-03 21:31:57 -0400

    fixed pages to match others pages style

---
+++ userPage/user_profile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+ * Change profile pic
+ */
+$("#edit-profile-pic").on("click", function() {
+    $("#popup-module-blackout").addClass("active");
+});
+
+/*
+* Change country flag
+*/
+$("#edit-flag").on("click", function() {
+   $("#popup-module-blackout").addClass("active");
+});
+
+/*
+ * Closes pop up window
+ * NOTE: some functionality is not yet present until we access to a server in phase 2 
+ */
+$("#confirm-changes").on("click", function() {
+    // In phase 2, once the user will be able to uplaod a new image
+    // Close the confirm changes popup
+    $(this).closest("#popup-module-blackout").removeClass("active");
+});
