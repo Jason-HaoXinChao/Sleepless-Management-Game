22ad7c2decd0a5be99f231a55e888406cf740b9b
Hao Xin Chao <hao.chao@mail.utoronto.ca>
2020-08-04 18:35:39 -0400

    Added hash function for credential password

--- models/UserCredential.js
+++ models/UserCredential.js
@@ -1,12 +1,31 @@
 /* user credential models */
+const bcrypt = require('bcryptjs');
 const mongoose = require("mongoose");
 
-const credentialSchema = new mongoose.Schema({
+const CredentialSchema = new mongoose.Schema({
     username: String,
     password: Number,
     email: String
 });
 
-const Credential = mongoose.model("Credential", credentialSchema);
+// A mongoose middleware to hash password before saving it into the database
+CredentialSchema.pre('save', function(next) {
+    const credential = this; // binds this to User document instance
+
+    // checks to ensure we don't hash password more than once
+    if (credential.isModified('password')) {
+        // generate salt and hash the password
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(credential.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+})
+
+const Credential = mongoose.model("Credential", CredentialSchema);
 
 module.exports = { Credential };
